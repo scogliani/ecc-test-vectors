e77ca48d1f5c3c2677888843d19ab5ae93026190
scogliani <simon.cogliani@gmail.com>
2020-10-26 13:56:27 +0100

    fix cmake and privatize mk_eckey

--- include/ecdh_kat.h
+++ include/ecdh_kat.h
@@ -3,11 +3,9 @@
 
 #include <openssl/ec.h>
 
-EC_KEY *mk_eckey(EC_GROUP* grp, const char *p);
-
 void ecdh_kat(EC_GROUP* group,
               const char* priv,
               const char* kcx0,
               const char* kcy0);
 
-#endif /* ECDH_KAT_H__ */ 
+#endif /* ECDH_KAT_H__ */
