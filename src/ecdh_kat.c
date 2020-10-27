28a2f0d8e8fbbdd0e94dea87f46be9a3bfda7cb8
scogliani <simon.cogliani@gmail.com>
2020-10-27 16:24:58 +0100

    Fix and refacto some code

--- src/ecdh_kat.c
+++ src/ecdh_kat.c
@@ -1,10 +1,9 @@
-#include <ecdh_kat.h>
-#include <utils.h>
-
 #include <openssl/bn.h>
 #include <openssl/ecdh.h>
 
-EC_KEY *mk_eckey(EC_GROUP* grp, const char *p);
+#include <ecdh_kat.h>
+#include <mk_eckey.h>
+#include <utils.h>
 
 void ecdh_kat(EC_GROUP* group,
               const char* priv,
@@ -67,51 +66,3 @@ void ecdh_kat(EC_GROUP* group,
   EC_KEY_free(key);
 }
 
-EC_KEY* mk_eckey(EC_GROUP* grp, const char* p)
-{
-  int ok = 0;
-  EC_KEY* k = NULL;
-  BIGNUM* priv = NULL;
-  EC_POINT* pub = NULL;
-
-  k = EC_KEY_new();
-  if (!k)
-    ABORT;
-
-  priv = BN_new();
-  if (!priv)
-    ABORT;
-
-  if (!EC_KEY_set_group(k, grp))
-    ABORT;
-
-  if (!BN_hex2bn(&priv, p))
-    ABORT;
-
-
-  if (!EC_KEY_set_private_key(k, priv))
-    ABORT;
-
-  pub = EC_POINT_new(grp);
-
-  if (!pub)
-    ABORT;
-
-  if (!EC_POINT_mul(grp, pub, priv, NULL, NULL, NULL))
-    ABORT;
-
-  if (!EC_KEY_set_public_key(k, pub))
-    ABORT;
-
-  ok = 1;
-
-  EC_POINT_free(pub);
-  BN_clear_free(priv);
-
-  if (ok)
-    return k;
-  else if (k)
-    EC_KEY_free(k);
-
-  return NULL;
-}
